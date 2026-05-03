9c6ad120e2623c3bfbf6174d984e456ca816a8ce
chartouis <erna95er@gmail.com>
2026-05-04 00:48:18 +0500

    refactor CreateAccountStage

--- src/main/java/yzarr/auth/pipeline/stages/CreateAccountStage.java
+++ src/main/java/yzarr/auth/pipeline/stages/CreateAccountStage.java
@@ -1,46 +1,28 @@
 package yzarr.auth.pipeline.stages;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
-import yzarr.auth.model.AuthException;
-import yzarr.auth.model.User;
-import yzarr.auth.model.enums.ErrorCode;
 import yzarr.auth.pipeline.AuthContext;
-import yzarr.auth.repo.UserRepo;
+import yzarr.auth.service.UserService;
 
 /* Creates a user, given the email and password are okay */
 @Component
 @Slf4j
 public class CreateAccountStage implements AuthStage {
 
-    private final BCryptPasswordEncoder encoder;
-    private final UserRepo repo;
+    private final UserService userService;
 
-    public CreateAccountStage(UserRepo repo, BCryptPasswordEncoder encoder) {
-        this.encoder = encoder;
-        this.repo = repo;
+    public CreateAccountStage(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public AuthContext process(AuthContext context) {
-        if (exists(context.getEmail())) {
-            throw new AuthException(ErrorCode.EMAIL_ALREADY_EXISTS);
-        }
-        User user = new User(context.getEmail(), encoder.encode(context.getPassword()));
-        if (!context.getProps().isEmailVerification()) {
-            user.setEmailVerified(true);
-        }
 
-        context.setUser(repo.save(user));
-        log.info("Created User : {}", user.getUsername());
+        context.setUser(userService.createUser(context.getEmail(), context.getPassword()));
+        log.info("Created User : {}", context.getUser().getUsername());
 
         return context;
     }
-
-    private boolean exists(String email) {
-        return repo.existsByEmail(email);
-    }
-
 }
--- src/main/java/yzarr/auth/service/UserService.java
+++ src/main/java/yzarr/auth/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import yzarr.auth.AuthProperties;
 import yzarr.auth.model.AuthException;
 import yzarr.auth.model.User;
 import yzarr.auth.model.enums.ErrorCode;
@@ -15,10 +16,12 @@ public class UserService {
 
     private final UserRepo userRepo;
     private final BCryptPasswordEncoder encoder;
+    private final AuthProperties props;
 
-    public UserService(UserRepo userRepo, BCryptPasswordEncoder encoder) {
+    public UserService(UserRepo userRepo, BCryptPasswordEncoder encoder, AuthProperties props) {
         this.userRepo = userRepo;
         this.encoder = encoder;
+        this.props = props;
     }
 
     public User findUser(String email) {
@@ -27,6 +30,24 @@ public class UserService {
             throw new AuthException(ErrorCode.INVALID_CREDENTIALS);
         }
         return ouser.get();
+    }
+
+    public boolean exists(String email) {
+        return userRepo.existsByEmail(email);
+    }
+
+    public void existsAndThrow(String email) {
+        if (exists(email)) {
+            throw new AuthException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        }
+    }
+
+    public User createUser(String email, String password) {
+        existsAndThrow(email);
+        String passwordHash = encoder.encode(password);
+        User user = new User(email, passwordHash);
+        user.setEmailVerified(!props.isEmailVerification());
+        return userRepo.save(user);
     }
 
     public boolean matches(String raw, String hash) {
