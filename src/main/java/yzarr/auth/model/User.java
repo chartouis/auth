73bbbc7390645e3113c690f9c0a192a509917063
chartouis <erna95er@gmail.com>
2026-05-07 20:04:59 +0500

    replace @Data to @Getter @Setter in User

--- src/main/java/yzarr/auth/model/User.java
+++ src/main/java/yzarr/auth/model/User.java
@@ -14,14 +14,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import yzarr.auth.model.enums.AuthProvider;
 import yzarr.auth.model.enums.Role;
 
-@Data
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 @NoArgsConstructor
 public class User {
     @Id
